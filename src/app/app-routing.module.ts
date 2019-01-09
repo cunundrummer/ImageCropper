e33f048b1f44cb940e7bb3134a7ade28fe744f6d
Unknown <sox_theguy@yahoo.com>
2019-01-09 16:00:30 -0500

    New Project
    Working with image cropping solutions.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/uppy', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [PageNotFoundComponent]
 })
 export class AppRoutingModule { }
